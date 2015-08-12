4d26315bea4fc7230b85871cdb1464b3e768cd49
Swastik Roy <swastik.roy@live.com>
2015-08-12 12:19:20 +0530

    Update flexFont.js

--- flexFont.js
+++ flexFont.js
@@ -1,8 +1,8 @@
 /*
-* flexFont.js v1.0
+* flexText.js v1.0
 * Copyright 2015 Swastik Roy, 
 *
-* flexFont.js by Swastik Roy
+* flexText.js by Swastik Roy
 * is licensed under the MIT License. Read a copy of the
 * license in the LICENSE.txt file
 
@@ -11,25 +11,82 @@
 
 (function($) {
 
- $.fn.flexText = function(settings) {
+ $.fn.flexFont = function(settings) {
 
-	var parameters = $.extend({
-	         maxFontSize   : 100,
-	         minFontSize   : 1,
-	         maxWidth   : 9999,
-	         minWidth   : 1,
-	         sizeRatio : 20,
+   var parameters = $.extend({
+            maxFontSize   : 100,
+            minFontSize   : 1,
+            maxWidth   : 9999,
+            minWidth   : 1,
+            sizeRatio : 20,
             ifparentClass: [null,1],
-            ifclass: null
-	      }, settings);
+            ifclass: null,
+            textEllipsis : false,  /* Values: atMinWidth, atMinFontSize, atAny */ 
+            overflowHidden: false
+         }, settings);
 
-	textChange = function(elem) {
+   textChange = function(elem) {
+         if(!($(elem).attr("data-flexfont")))$(elem).attr("data-flexfont",$(elem).text());
+         $(elem).text($(elem).attr("data-flexfont"));
             parent_width = $(elem).width();
-            width = parent_width > parameters.maximum ? parameters.maximum : (parent_width < parameters.minimum ? parameters.minimum : parent_width);
+            width = parent_width > parameters.maxWidth ? parameters.maxWidth : parent_width < parameters.minWidth ? parameters.minWidth : parent_width;
             fontSize0 = width / parameters.sizeRatio;
             fontSize = fontSize0 > parameters.maxFontSize ? parameters.maxFontSize : (fontSize0 < parameters.minFontSize ? parameters.minFontSize : fontSize0);
-         	$(elem).css('font-size', fontSize + 'px');
+            $(elem).css('font-size', fontSize + 'px');
+
+           
+            if(parameters.textEllipsis == "atMinWidth"){
+               if(parent_width<parameters.minWidth){
+                  var c = countVisibleCharacters(elem);
+                  $(elem).text(shorten($(elem).attr("data-flexfont"),c+3));
+               }            
+            }
+            else if(parameters.textEllipsis == "atMinFontSize"){
+               if(fontSize0<parameters.minFontSize){
+                  var c = countVisibleCharacters(elem);
+                  $(elem).text(shorten($(elem).attr("data-flexfont"),c+3));
+               }
+            }
+            else if(parameters.textEllipsis == "atAny"){
+                  var c = countVisibleCharacters(elem);
+                  $(elem).text(shorten($(elem).attr("data-flexfont"),c+3));
+            }
+
+             if(parameters.overflowHidden)$(elem).css('overflow',"hidden");
+             
+            
+         
       };
+
+
+         function countVisibleCharacters(element) {
+          var text = element.firstChild.nodeValue;
+          var r = 0;
+          
+          element.removeChild(element.firstChild);
+          
+          for(var i = 0; i < text.length; i++) {
+              var newNode = document.createElement('span');
+              newNode.appendChild(document.createTextNode(text.charAt(i)));
+              element.appendChild(newNode);
+              
+              if(newNode.offsetLeft < element.offsetWidth) {
+                  r++;
+              }
+          }
+          
+          return r;
+      }
+
+      function shorten(text, maxLength) {
+             var ret = text;
+             if (ret.length > maxLength) {
+                 ret = ret.substr(0,maxLength-3) + "...";
+             }
+             return ret;
+         }
+      
+
 
 
       
@@ -38,7 +95,6 @@
          $(window).resize(function(){
 
             var condition = true;
-            console.log(parameters.ifparentClass);
             condition = (parameters.ifclass != null) ? ($(this2).hasClass(parameters.ifClass) ? true : false) : condition;
             if(parameters.ifparentClass[0] != null){
                var $target_elem = $(this2);
@@ -54,6 +110,9 @@
          textChange(this);
       });
       
+     
+
+
  };
 
 
